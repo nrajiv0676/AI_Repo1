ef129fe9e5dcf0870741f25823a969d5f00a7bcd
nrajiv0676 <nrajiv0676@gmail.com>
2021-06-28 19:01:52 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -44,6 +44,10 @@ def heartstrokepredict():
 def brainTumorPredict():
     return render_template("brain_tumor_predict.html")
 
+@app.route("/MalariaPredict")
+def malariaPredict():
+    return render_template("malaria_predict.html")
+
 
 @app.route("/EmailSpamClassify")
 def emailSpamClassify():
@@ -180,7 +184,7 @@ def brainTumorPredictResult():
 
         # Save the file to ./uploads
         basepath = os.path.dirname(__file__)
-        file_path = os.path.join(basepath, 'models/uploads', secure_filename(f.filename))
+        file_path = os.path.join(basepath, 'uploads', secure_filename(f.filename))
         f.save(file_path)
 
         print(file_path)
@@ -211,6 +215,108 @@ def brainTumorPredictResult():
         #print(np.argmax(preds))
         label_list = ['Healthy', 'Tumor']
         result = label_list[np.argmax(preds)]
+        #result = label_list[preds]
+        print(result)
+
+        return result
+    return None
+
+
+######## Model Evaluation for Malaria Prediction #####################
+
+def get_malariaModel():
+    global malaria_model
+    #model = load_model('.h5')
+
+    #json_file = open('models/brtumor_model3.json', 'r')
+    #loaded_model_json = json_file.read()
+    #json_file.close()
+    #brTumor_model = model_from_json(loaded_model_json)
+
+    #malaria_model.load_weights("models/malaria_prediction_model.h5")
+    malaria_model=load_model("models/malaria_prediction_model.h5")
+
+    #brTumor_model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+    print('malaria model loaded')
+
+def malaria_model_predict(img_path, model):
+
+    print('inside malaria model_predict function')
+    #img = image.load_img(img_path, target_size=(180, 180))
+    #img = image.load_img(img_path, target_size=(50, 50, 3))
+    data = image.load_img(img_path, target_size=(50, 50, 3))
+    data = np.expand_dims(data, axis=0)
+    data = data * 1.0 / 255
+
+    # Preprocessing the image
+    #x = image.img_to_array(img)
+    #x = np.true_divide(x, 255)
+    #x = np.expand_dims(x, axis=0)
+
+    # Be careful how your trained model deals with the input
+    # otherwise, it won't make correct prediction!
+    #x = preprocess_input(x, mode='caffe')
+    #x = preprocess_input(x)
+
+
+    preds = model.predict(data)
+    print('preds in malaria_model_predict')
+    print(preds)
+    return np.argmax(preds, axis=1)
+
+@app.route('/malariaPredictResult', methods=['GET', 'POST'])
+def malariaPredictResult():
+    if request.method == 'POST':
+        get_malariaModel()
+        # Get the file from post request
+        print('inside malariaPredictResult upload function')
+        f = request.files['file']
+
+        print(f)
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        print(file_path)
+
+        #tmp_filepath = "C:\Rajiv\work\DataScience\BrainCancer_Dataset\Brain Tumor Data Set\htn1.jpg"
+        #tmp_filepath = "C:\\Rajiv\\work\\DataScience\\BrainCancer_Dataset\\Brain Tumor Data Set\\htn1.jpg"
+        #print(tmp_filepath)
+
+        #file_path2 = os.path.join(os.path.dirname(__file__), 'testimages', f.filename)
+        #print(file_path2)
+
+        #json_file = open('model.json', 'r')
+        #loaded_model_json = json_file.read()
+        #json_file.close()
+        #loaded_model = model_from_json(loaded_model_json)
+
+        #loaded_model.load_weights("model.h5")
+
+        # Make prediction
+        preds = malaria_model_predict(file_path, malaria_model)
+
+        # Process your result for human
+        # pred_class = preds.argmax(axis=-1)            # Simple argmax
+        #pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
+        #result = str(pred_class[0][0][1])               # Convert to string
+        print('Predicted Malaria result Values in result function: ')
+        print(preds)
+        #print(np.argmax(preds))
+        #label_list = ['Healthy', 'Tumor']
+        #result = label_list[np.argmax(preds)]
+
+        #indices = {0: 'PARASITIC', 1: 'Uninfected', 2: 'Invasive carcinomar', 3: 'Normal'}
+
+        #predicted_class = np.asscalar(np.argmax(preds, axis=1))
+        #accuracy = round(result[0][predicted_class] * 100, 2)
+        indices = ['PARASITIC', 'Uninfected']
+        result = indices[np.asscalar(preds)]
+
+
         #result = label_list[preds]
         print(result)
 
